04d8a6d54e153cd28df6ac7e255552b0f2e928cb
aidosgal <agalimzhan928@gmail.com>
2025-01-05 16:58:00 +0500

    fix(organization): add user_id to request

--- internal/handler/organization_handler.go
+++ internal/handler/organization_handler.go
@@ -1,33 +1,43 @@
 package handler
 
 import (
+	"fmt"
 	"log/slog"
 	"net/http"
 
 	"github.com/2group/2sales.core-service/internal/grpc"
 	organizationv1 "github.com/2group/2sales.core-service/pkg/gen/go/organization"
 	"github.com/2group/2sales.core-service/pkg/json"
+	middleware "github.com/2group/2sales.core-service/pkg/middeware"
 )
 
 type OrganizationHandler struct {
-    log *slog.Logger
-    organization *grpc.OrganizationClient
+	log          *slog.Logger
+	organization *grpc.OrganizationClient
 }
 
-func NewOrganizationHandler(organization *grpc.OrganizationClient) *OrganizationHandler{
-    return &OrganizationHandler{organization: organization}
+func NewOrganizationHandler(organization *grpc.OrganizationClient) *OrganizationHandler {
+	return &OrganizationHandler{organization: organization}
 }
 
-func (h *OrganizationHandler) CreateOrganization (w http.ResponseWriter, r *http.Request) {
-    req := &organizationv1.CreateOrganizationRequest{}
-    json.ParseJSON(r, &req) 
+func (h *OrganizationHandler) CreateOrganization(w http.ResponseWriter, r *http.Request) {
+	user_id, ok := middleware.GetUserID(r)
+	if !ok {
+		json.WriteError(w, http.StatusBadRequest, fmt.Errorf("Unauthorized"))
+		return
+	}
 
-    response, err := h.organization.Api.CreateOrganization(r.Context(), req)
-    if err != nil {
-        json.WriteError(w, http.StatusInternalServerError, err)
-        return
-    }
+	req := &organizationv1.CreateOrganizationRequest{}
+	json.ParseJSON(r, &req)
 
-    json.WriteJSON(w, http.StatusCreated, response)
-    return
+    req.UserId = int64(user_id)
+
+	response, err := h.organization.Api.CreateOrganization(r.Context(), req)
+	if err != nil {
+		json.WriteError(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	json.WriteJSON(w, http.StatusCreated, response)
+	return
 }
