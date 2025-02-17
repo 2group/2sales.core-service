e303ef8fec5d4b85b5ac99f49dd62d16ac02a495
aidosgal <agalimzhan928@gmail.com>
2025-02-17 17:32:09 +0500

    feat(logging): add logging to crm service

--- internal/handler/crm_handler.go
+++ internal/handler/crm_handler.go
@@ -20,10 +20,16 @@ type CrmHandler struct {
 }
 
 func NewCrmHandler(log *slog.Logger, crm *grpc.CrmClient) *CrmHandler {
-	return &CrmHandler{log: log, crm: crm}
+	return &CrmHandler{
+		log: log,
+		crm: crm,
+	}
 }
 
 func (h *CrmHandler) CreateLead(w http.ResponseWriter, r *http.Request) {
+	ci, _ := middleware.GetCorrelationID(r.Context())
+	log := h.log.With("method", "CreateLead", "correlation_id", ci)
+	
 	organizationID, ok := middleware.GetOrganizationID(r)
 	if !ok {
 		json.WriteError(w, http.StatusUnauthorized, fmt.Errorf("unauthorized"))
@@ -36,62 +42,67 @@ func (h *CrmHandler) CreateLead(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	h.log.Info("create lead handler", "organization_id", organizationID, "user_id", userID)
-
 	req := &crmv1.CreateLeadRequest{}
 	if err := json.ParseJSON(r, req); err != nil {
+		log.Error("Failed to parse json", "error", err)
 		json.WriteError(w, http.StatusBadRequest, err)
 		return
 	}
-
 	req.Lead.CreatedByUser = &userv1.User{}
 
 	req.Lead.CreatedByOrganizationId = &organizationID
 	req.Lead.CreatedByUser.Id = &userID
-
+	
+	log.Info("Creating lead", "request", req)
 	response, err := h.crm.Api.CreateLead(r.Context(), req)
 	if err != nil {
+		log.Error("Failed to create lead", "error", err)
 		json.WriteError(w, http.StatusBadRequest, err)
 		return
 	}
 
-	// 5. Respond with the newly created lead
+	log.Info("Successfully created lead", "response", response)
 	json.WriteJSON(w, http.StatusCreated, response)
+	return
 }
 
 func (h *CrmHandler) GetLead(w http.ResponseWriter, r *http.Request) {
+	ci, _ := middleware.GetCorrelationID(r.Context())
+	log := h.log.With("method", "GetLead", "correlation_id", ci)
 	leadIDStr := chi.URLParam(r, "lead_id")
-
-	h.log.Info("get lead handler", "lead_id", leadIDStr)
 
 	leadID, err := strconv.ParseInt(leadIDStr, 10, 64)
 	if err != nil {
-		h.log.Error("invalid lead_id format", "lead_id", leadIDStr, "error", err)
+		log.Error("Invalid lead_id format", "lead_id", leadIDStr, "error", err)
 		json.WriteError(w, http.StatusBadRequest, err)
 		return
 	}
-
-	// Create the GetLeadRequest
+	
 	req := &crmv1.GetLeadRequest{LeadId: leadID}
-
+	log.Info("Getting lead", "request", req)
+	
 	response, err := h.crm.Api.GetLead(r.Context(), req)
 	if err != nil {
+		log.Error("Failed to get lead", "error", err)
 		json.WriteError(w, http.StatusBadRequest, err)
 		return
 	}
 
-	// 5. Respond with the newly created lead
+	log.Info("Successfuly created lead", "response", response)
 	json.WriteJSON(w, http.StatusCreated, response)
+	return
 }
 
 func (h *CrmHandler) UpdateLead(w http.ResponseWriter, r *http.Request) {
+	ci, _ := middleware.GetCorrelationID(r.Context())
+	log := h.log.With("method", "UpdateLead", "correlation_id", ci)
 	leadIDStr := chi.URLParam(r, "lead_id")
 
-	h.log.Info("update lead handler", "lead_id", leadIDStr)
+	log.Info("Update lead handler", "lead_id", leadIDStr)
 
 	leadID, err := strconv.ParseInt(leadIDStr, 10, 64)
 	if err != nil {
-		h.log.Error("invalid lead_id format", "lead_id", leadIDStr, "error", err)
+		log.Error("Invalid lead_id format", "lead_id", leadIDStr, "error", err)
 		json.WriteError(w, http.StatusBadRequest, err)
 		return
 	}
@@ -103,80 +114,94 @@ func (h *CrmHandler) UpdateLead(w http.ResponseWriter, r *http.Request) {
 	}
 
 	req.Lead.LeadId = &leadID
+	log.Info("Updating lead", "request", req)
 
 	response, err := h.crm.Api.UpdateLead(r.Context(), req)
 	if err != nil {
+		log.Error("Failed to update lead", "error", err)
 		json.WriteError(w, http.StatusBadRequest, err)
 		return
 	}
 
+	log.Info("Successfully updated lead", "response", response)
 	json.WriteJSON(w, http.StatusCreated, response)
 }
 
 func (h *CrmHandler) PatchLead(w http.ResponseWriter, r *http.Request) {
+	ci, _ := middleware.GetCorrelationID(r.Context())
+	log := h.log.With("method", "PatchLead", "correlation_id", ci)
 	leadIDStr := chi.URLParam(r, "lead_id")
 
-	h.log.Info("get lead handler", "lead_id", leadIDStr)
+	log.Info("Get lead handler", "lead_id", leadIDStr)
 
 	leadID, err := strconv.ParseInt(leadIDStr, 10, 64)
 	if err != nil {
-		h.log.Error("invalid lead_id format", "lead_id", leadIDStr, "error", err)
+		log.Error("Invalid lead_id format", "lead_id", leadIDStr, "error", err)
 		json.WriteError(w, http.StatusBadRequest, err)
 		return
 	}
 
 	req := &crmv1.PatchLeadRequest{}
 	if err := json.ParseJSON(r, req); err != nil {
+		log.Error("Failed to parse json", "error", err)
 		json.WriteError(w, http.StatusBadRequest, err)
 		return
 	}
 
 	req.Lead.LeadId = &leadID
+	log.Info("Patching lead", "request", req)
 
 	response, err := h.crm.Api.PatchLead(r.Context(), req)
 	if err != nil {
+		log.Error("Failed to patch lead", "error", err)
 		json.WriteError(w, http.StatusBadRequest, err)
 		return
 	}
 
+	log.Info("Successfully patched lead", "response", response)
 	json.WriteJSON(w, http.StatusCreated, response)
 }
 
 func (h *CrmHandler) DeleteLead(w http.ResponseWriter, r *http.Request) {
+	ci, _ := middleware.GetCorrelationID(r.Context())
+	log := h.log.With("method", "DeleteLead", "correlation_id", ci)
 	leadIDStr := chi.URLParam(r, "lead_id")
 
-	h.log.Info("delete lead handler", "lead_id", leadIDStr)
+	log.Info("delete lead handler", "lead_id", leadIDStr)
 
 	leadID, err := strconv.ParseInt(leadIDStr, 10, 64)
 	if err != nil {
-		h.log.Error("invalid lead_id format", "lead_id", leadIDStr, "error", err)
+		log.Error("invalid lead_id format", "lead_id", leadIDStr, "error", err)
 		json.WriteError(w, http.StatusBadRequest, err)
 		return
 	}
 
-	// Create the GetLeadRequest
 	req := &crmv1.DeleteLeadRequest{LeadId: leadID}
-
+	log.Info("Deleting lead", "request", req)
+	
 	response, err := h.crm.Api.DeleteLead(r.Context(), req)
 	if err != nil {
+		log.Error("Failed to delete the lead", "error", err)
 		json.WriteError(w, http.StatusBadRequest, err)
 		return
 	}
 
-	// 5. Respond with the newly created lead
+	log.Info("Successfully deleted the lead", "response", response)
 	json.WriteJSON(w, http.StatusCreated, response)
+	return
 }
 
 func (h *CrmHandler) ListLeads(w http.ResponseWriter, r *http.Request) {
-	// 1. Get organization ID from your middleware
+	ci, _ := middleware.GetCorrelationID(r.Context())
+	log := h.log.With("method", "ListLeads", "correlation_id", ci)
+	
 	organizationID, ok := middleware.GetOrganizationID(r)
 	if !ok {
 		json.WriteError(w, http.StatusUnauthorized, fmt.Errorf("unauthorized"))
 		return
 	}
-	h.log.Info("list leads handler", "organization_id", organizationID)
+	log.Info("list leads handler", "organization_id", organizationID)
 
-	// 2. Parse query parameters for limit and offset
 	query := r.URL.Query()
 	limitParam := query.Get("limit")
 	offsetParam := query.Get("offset")
@@ -186,7 +211,6 @@ func (h *CrmHandler) ListLeads(w http.ResponseWriter, r *http.Request) {
 		offset int32
 	)
 
-	// Default limit if none provided (e.g., 10)
 	if limitParam == "" {
 		limit = 10
 	} else {
@@ -207,20 +231,20 @@ func (h *CrmHandler) ListLeads(w http.ResponseWriter, r *http.Request) {
 		offset = int32(parsedOffset)
 	}
 
-	// 3. Build the gRPC request
 	req := &crmv1.ListLeadsRequest{
 		OrganizationId: organizationID,
 		Limit:          limit,
 		Offset:         offset,
 	}
+	log.Info("Listing leads", "request", req)
 
-	// 4. Call the CRM service
 	response, err := h.crm.Api.ListLeads(r.Context(), req)
 	if err != nil {
+		log.Error("Failed to list leads", "error", err)
 		json.WriteError(w, http.StatusBadRequest, err)
 		return
 	}
 
-	// 5. Return the leads JSON response
+	log.Info("Successfully listed leads", "response", response)
 	json.WriteJSON(w, http.StatusOK, response)
 }
